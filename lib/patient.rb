01675ac01491b2057a769a18e4ae7c8e40551f6e
Laura Trager <laura.trager@gmail.com>
2017-12-01 16:43:26 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,12 @@
+class Patient
+
+  attr_accessor :name, :appointment, :doctor
+
+  def initialize(name, doctor)
+    @name = name
+    doctor.patients << self
+    @appointment = []
+  end
+
+  
+    
